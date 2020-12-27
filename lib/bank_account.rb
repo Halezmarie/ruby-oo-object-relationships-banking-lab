5d3f55cff85d02ca7e621bc2f13e97c31f13f0be
Halezmarie <teacherhaleymarie@gmail.com>
2020-12-27 16:47:52 -0600

    commit tree 295bef69f6ce73cb75f5898a98111d62110f9782

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,29 @@
 class BankAccount
-
-end
+    attr_reader :name
+    attr_accessor :balance, :status
+  
+    def initialize(name)
+      @name = name
+      @balance = 1000
+      @status = "open"
+    end
+  
+    def deposit(amount)
+      @balance += amount
+    end
+  
+    def display_balance
+      "Your balance is $#{@balance}."
+    end
+  
+    def valid? #seeing if it can be taken out
+      @status == "open" && @balance > 0
+    end
+  
+    def close_account
+      @status = "closed"
+    end
+  
+    private
+    attr_writer :name
+  end
